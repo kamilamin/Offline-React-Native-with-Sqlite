aa150020ec7674691de2bd2a94c48078380976d0
Kamil Amin <kamil.amin@mparsec.com>
2019-05-13 16:08:09 +0500

    --Commit: Product Details Screen is in progress

---
+++ components/ProjectDetailsScreen.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import { Button, View, Text } from "react-native";
+
+export default class ProductDetailsScreen extends Component {
+    static navigationOptions = {
+        title: 'Product Details',
+    };
+    render() {
+        return (
+            <View>
+                
+            </View>
+        )
+    }
+}
