b491cf7727237cbc0dcc2280e6a2c833050a5a64
kamil amin <kamil.amin@live.co.uk>
2019-05-14 09:58:14 -0700

    --Commit: Formating the code of product details component

--- components/ProjectDetailsScreen.js
+++ components/ProjectDetailsScreen.js
@@ -1,27 +1,27 @@
-import React, { Component } from "react";
-import { Button, View, Text } from "react-native";
+import React, {Component} from 'react';
+import {Button, View, Text} from 'react-native';
 
 export default class ProductDetailsScreen extends Component {
-    static navigationOptions = {
-        title: 'Product Details',
-    };
-    render() {
-        return (
-            <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-                <Text>Add Product</Text>
-                <Button  
-                    title="Go to Add Product . . . Again"
-                    onPress={() => this.props.navigation.navigate('AddProduct')}
-                />
-                <Button 
-                    title="Go to Home"
-                    onPress={() => this.props.navigation.navigate('Product')}
-                />
-                <Button 
-                    title="Go Back"
-                    onPress={() => this.props.navigation.goBack()}
-                />
-            </View>
-        )
-    }
+  static navigationOptions = {
+    title: 'Product Details',
+  };
+  render () {
+    return (
+      <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+        <Text>Add Product</Text>
+        <Button
+          title="Go to Add Product . . . Again"
+          onPress={() => this.props.navigation.navigate ('AddProduct')}
+        />
+        <Button
+          title="Go to Home"
+          onPress={() => this.props.navigation.navigate ('Product')}
+        />
+        <Button
+          title="Go Back"
+          onPress={() => this.props.navigation.goBack ()}
+        />
+      </View>
+    );
+  }
 }
