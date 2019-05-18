b4f4922b7071f906d3b6d101da6093a8f8036de5
kamil amin <kamil.amin@live.co.uk>
2019-05-18 04:34:52 -0700

    --Commit: Updating routing and connected with screen

--- App.js
+++ App.js
@@ -7,42 +7,47 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
+import {createAppContainer, createStackNavigator} from 'react-navigation';
+import ProductScreen from './components/ProductScreen';
+import ProductDetailsScreen from './components/ProductDetailsScreen';
+import AddProductScreen from './components/ProductAddScreen';
+import EditProductScrren from './components/ProductEditScreen';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+const RootStack = createStackNavigator (
+  {
+    Product: ProductScreen,
+    ProductDetails: ProductDetailsScreen,
+    AddProduct: AddProductScreen,
+    EditProduct: EditProductScrren,
+  },
+  {
+    initialRouteName: 'Product',
+    navigationOPtions: {
+      headerStyle: {
+        backgroundColor: '#777777',
+      },
+      headerTintColor: '#fff',
+      headerTitleStyle: {
+        fontWeight: 'bold',
+      },
+    },
+  }
+);
+
+const RootContainer = createAppContainer (RootStack);
 
 export default class App extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
-    );
+  render () {
+    return <RootContainer />;
   }
 }
 
-const styles = StyleSheet.create({
+const styles = StyleSheet.create ({
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
   },
 });
